717c5d3e0518bc374881845acd09f7216b938866
lexxy <lexxy23@gmail.com>
2009-04-18 14:49:57 +0000

    - Checkin of the EJB components
    git-svn-id: http://arida.googlecode.com/svn/trunk@5 62843f1a-2b3c-11de-958b-d3ca58845339

---
+++ arida/ejb/persistence/src/main/java/ds2/arida/business/persistence/entity/AccountEntity.java
@@ -0,0 +1,76 @@
+/**
+ * Arida Raid and Clan Management
+ * Copyright (C) 2009  Dirk Strauss
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ds2.arida.business.persistence.entity;
+
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+import ds2.arida.common.api.Account;
+import ds2.arida.common.api.Character;
+import ds2.arida.common.api.NotificationType;
+
+/**
+ * The accounts
+ * 
+ * @author kaeto23
+ * 
+ */
+@Entity(name = "Account")
+@Table(name = "arida_accounts")
+public class AccountEntity implements Account {
+
+	/**
+	 * 
+	 */
+	public AccountEntity() {
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public List<Character> getChars() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public long getId() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public String getNotificationInfo(NotificationType t) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getOpenId() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean isLocked() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
